cbc82ef58f6670b07e57588490bed5ca1d77366d
22DP2AKirs <131154529+22DP2AKirs@users.noreply.github.com>
2024-03-14 19:43:02 +0200

    Update Degviela.cs

--- Degviela.cs
+++ Degviela.cs
@@ -1,22 +1,35 @@
 using System;
 public abstract class Degviela
 {
-    // Mainīgie.
+    // Mainīgie (lauki):
     protected string nosaukums;
     protected string krasa;
     protected double cenaParLitru;
     protected int autoDegvielasPaterinaEfektivitate; // Cik auto patērē par 100 km.
-    // Metodes.
-    /// Abstraktās:
-    public abstract void DemonstretEfektivitati(int distanceKilometros); // Parāda cik lielu attālumu var nobraukt ar noteikto degvielu un tās daudzumu. /* 0 -------------::------ 100*/
-    public abstract double AprekinatCikLitrusVajadzesPrieksDotasDistances(int distanceKilometros);
-    public abstract double nobraucamieKilometriParLitruDegvielas();
     
-    // Virtuālās:
+    // Īpašības:
+    public string Nosaukums { get => nosaukums; set => nosaukums = value; }
+    public string Krasa { get => krasa; set => krasa = value; }
+    public double CenaParLitru { get => cenaParLitru; set => cenaParLitru = value; }
+    public int AutoDegvielasPaterinaEfektivitate { get => autoDegvielasPaterinaEfektivitate; set => autoDegvielasPaterinaEfektivitate = value; }
+    
+    // Metodes:
+    /// Abstraktās:
+    public abstract void AprekinatDegvieluDistancei(int distanceKilometros);
+    public abstract double PateretaDegvielaParVienuKilometru();
+    
+    /// Virtuālās:
     public virtual void Informacija() // Parāda informāciju par degvielu.
     {
         Console.WriteLine("--------------------Informācija-------------------------");
         Console.WriteLine("nosaukums: {0}, krasa: {1}, cena litrā: {2} EUR",
         nosaukums, krasa, cenaParLitru);
+    }
+    
+    /// Parastā metode:
+    public void AprekinatCenuBraucienam(int distance)
+    {
+        Console.WriteLine($"-----------Cena priekš brauciena ({distance} km.)-------------");
+        Console.WriteLine("Cena: " + Math.Round(cenaParLitru * (distance * PateretaDegvielaParVienuKilometru()), 2) + " Eiro.");
     }
 }
