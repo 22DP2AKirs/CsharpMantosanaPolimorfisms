7a7c2c2e43c8815832aac0439c7d88c9c490c04a
22DP2AKirs <131154529+22DP2AKirs@users.noreply.github.com>
2024-03-15 14:17:21 +0200

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -8,13 +8,13 @@ class HelloWorld {
     Degviela degviela;
     
     // Bendzīns:
-    degviela = new Benzins(9); // Patēriņš par 100 km.
+    degviela = new Benzins(9, "E95"); // Patēriņš par 100 km.
     degviela.Informacija();
     degviela.AprekinatDegvieluDistancei(460);
     degviela.AprekinatCenuBraucienam(460);
     
     // Dīzeļdegviela:
-    degviela = new Dizeldegviela(9); // Patēriņš par 100 km.
+    degviela = new Dizeldegviela(9, "95"); // Patēriņš par 100 km.
     degviela.Informacija();
     degviela.AprekinatDegvieluDistancei(460);
     degviela.AprekinatCenuBraucienam(460);
