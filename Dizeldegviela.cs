b1012783adfdef3a5c9e9542ba6052688539c1a2
22DP2AKirs <131154529+22DP2AKirs@users.noreply.github.com>
2024-03-15 14:17:03 +0200

    Update Dizeldegviela.cs

--- Dizeldegviela.cs
+++ Dizeldegviela.cs
@@ -1,15 +1,49 @@
 using System;
 public class Dizeldegviela : Degviela
 {
-    public Dizeldegviela(int autoDegvielasPaterinaEfektivitate)
+    public Dizeldegviela(int autoDegvielasPaterinaEfektivitate, string dizeldegvielasTips)
     {
+        /* Dīzeļdevielas varianti:  95, 98, DD*/
         // 20% Labāka efektivitāte par bendzīnu.
-        nosaukums = "dīzelis";
+        dizeldegvielasTips.ToUpper();
+        if (dizeldegvielasTips.Equals("95"))
+        {
+            nosaukums = "Neste Futura 95";
+            cenaParLitru = 1.68;
+        }
+        else if (dizeldegvielasTips.Equals("98"))
+        {
+            nosaukums = "Neste Futura 98";
+            cenaParLitru = 1.73;
+        }
+        else if (dizeldegvielasTips.Equals("DD"))
+        {
+            nosaukums = "Neste Futura D";
+            cenaParLitru = 1.66;
+        }
+        else if (dizeldegvielasTips.Equals("PRO D"))
+        {
+            nosaukums = "Neste Pro Diesel";
+            cenaParLitru = 1.77;
+        }
+        else
+        {
+            nosaukums = "NoName";
+            cenaParLitru = 0;
+        }
         krasa = "oranžs";
-        cenaParLitru = 1.82; // Lētāks par bendzīnu.
         this.autoDegvielasPaterinaEfektivitate = autoDegvielasPaterinaEfektivitate; // x litri par 100 km
     }
     
+    // Aprēķini bendzīntankam.
+    
+    
+    
+    
+    
+    
+    
+    // Aprēķini mašīnai.
     /// Abstraktie override:
     public override void AprekinatDegvieluDistancei(int distanceKilometros)
     {
@@ -18,7 +52,6 @@ public class Dizeldegviela : Degviela
         Console.WriteLine($"----------------Degviela priekš {distanceKilometros} km.-----------------");
         Console.WriteLine(PateretaDegvielaParVienuKilometru() * distanceKilometros + " Litri.");
     }
-    
     
     public override double PateretaDegvielaParVienuKilometru()
     {
