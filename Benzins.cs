6065f4f3a3be23ce79548ca90d02192a03500e66
22DP2AKirs <131154529+22DP2AKirs@users.noreply.github.com>
2024-03-17 21:10:30 +0200

    Update Benzins.cs

--- Benzins.cs
+++ Benzins.cs
@@ -1,32 +1,56 @@
 using System;
 public class Benzins : Degviela
 {
-    public Benzins(int autoDegvielasPaterinaEfektivitate, string dizeldegvielasTips)
+    public Benzins(string dizeldegvielasTips, int autoDegvielasPaterinaEfektivitate = 10)
     {
         /* Bendzīna varianti:  E95, E98*/
-        nosaukums = "benzīns";
+        // Dārgāks par dīzeli.
+        nosaukums = "Benzīns ";
         dizeldegvielasTips.ToUpper();
         if (dizeldegvielasTips.Equals("E95"))
         {
-            nosaukums = "Neste Futura 95";
-            cenaParLitru = 1.68;
+            nosaukums += "E95";
+            cenaParLitru = 1.92;
         }
         else if (dizeldegvielasTips.Equals("E98"))
         {
-            nosaukums = "Neste Futura 98";
-            cenaParLitru = 1.73;
+            nosaukums += "E98";
+            cenaParLitru = 1.99;
         }
         else
         {
             nosaukums = "NoName";
             cenaParLitru = 0;
         }
-        krasa = "dzeltanīgs";
-        cenaParLitru = 2.04; // Dārgāks par dīzeli.
+        krasa = "Dzeltanīgs";
         this.autoDegvielasPaterinaEfektivitate = autoDegvielasPaterinaEfektivitate; // x litri par 100 km
-
+    }
+     
+    // Aprēķini bendzīntankam.
+    /// Abstraktie override:
+    public override void PircejuTabula(int maksimalaisPircejuSkKatramPumpim, int aptuvenieBakasIzmeri)
+    {
+        if (maksimalaisPircejuSkKatramPumpim > 0 && aptuvenieBakasIzmeri > 15)
+        {
+            // Izveido objektus, lai aprēķinos ņemtu to cenas:
+            Benzins E95 = new Benzins("E95");
+            Benzins E98 = new Benzins("E98");
+            
+            // Sāk izvadīt tabulu:
+            Console.WriteLine("-------Paredzētā peļņa no benzīna-------");
+            Console.WriteLine("| Diena |   E95   |   E98   |");
+            for(int i = 0 ; i < 7 ; i++)
+            {
+                Console.WriteLine($"|   {i + 1}.  | {E95.AprekinatTabulasCenas(8, maksimalaisPircejuSkKatramPumpim, aptuvenieBakasIzmeri)}| {E98.AprekinatTabulasCenas(8, maksimalaisPircejuSkKatramPumpim, aptuvenieBakasIzmeri)}|");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Pārāk maz pircēju vai pārāk mazs bākas izmērs!");
+        }
     }
     
+    // Aprēķini mašīnai.
     /// Abstraktie override:
     public override double PateretaDegvielaParVienuKilometru()
     {
@@ -43,7 +67,7 @@ public class Benzins : Degviela
         Console.WriteLine(PateretaDegvielaParVienuKilometru() * distanceKilometros + " Litri.");
     }
     
-    /// Virtuālie override:
+    // Virtuālie override:
     public override void Informacija()
     {
         base.Informacija();
