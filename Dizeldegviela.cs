a617c70b1d045247f911304c1ba8f3ca9e9ade9a
22DP2AKirs <131154529+22DP2AKirs@users.noreply.github.com>
2024-03-17 21:11:07 +0200

    Update Dizeldegviela.cs

--- Dizeldegviela.cs
+++ Dizeldegviela.cs
@@ -1,47 +1,65 @@
 using System;
 public class Dizeldegviela : Degviela
 {
-    public Dizeldegviela(int autoDegvielasPaterinaEfektivitate, string dizeldegvielasTips)
+    public Dizeldegviela(string dizeldegvielasTips, int autoDegvielasPaterinaEfektivitate = 10)
     {
         /* Dīzeļdevielas varianti:  95, 98, DD*/
         // 20% Labāka efektivitāte par bendzīnu.
+        nosaukums = "Dīzelis ";
         dizeldegvielasTips.ToUpper();
         if (dizeldegvielasTips.Equals("95"))
         {
-            nosaukums = "Neste Futura 95";
+            nosaukums += "Neste Futura 95";
             cenaParLitru = 1.68;
         }
         else if (dizeldegvielasTips.Equals("98"))
         {
-            nosaukums = "Neste Futura 98";
+            nosaukums += "Neste Futura 98";
             cenaParLitru = 1.73;
         }
         else if (dizeldegvielasTips.Equals("DD"))
         {
-            nosaukums = "Neste Futura D";
+            nosaukums += "Neste Futura D";
             cenaParLitru = 1.66;
         }
-        else if (dizeldegvielasTips.Equals("PRO D"))
+        else if (dizeldegvielasTips.Equals("PRO"))
         {
-            nosaukums = "Neste Pro Diesel";
+            nosaukums += "Neste Pro Diesel";
             cenaParLitru = 1.77;
         }
         else
         {
-            nosaukums = "NoName";
+            nosaukums += "NoName";
             cenaParLitru = 0;
         }
-        krasa = "oranžs";
+        krasa = "Oranžs";
         this.autoDegvielasPaterinaEfektivitate = autoDegvielasPaterinaEfektivitate; // x litri par 100 km
     }
     
     // Aprēķini bendzīntankam.
-    
-    
-    
-    
-    
-    
+    public override void PircejuTabula(int maksimalaisPircejuSkKatramPumpim, int aptuvenieBakasIzmeri)
+    {
+        if (maksimalaisPircejuSkKatramPumpim > 0 && aptuvenieBakasIzmeri > 15)
+        {
+            // Izveido objektus, lai aprēķinos ņemtu to cenas:
+            Dizeldegviela D95 = new Dizeldegviela("95");
+            Dizeldegviela D98 = new Dizeldegviela("98");
+            Dizeldegviela DD = new Dizeldegviela("DD");
+            Dizeldegviela PRO = new Dizeldegviela("PRO");
+            
+            // Sāk izvadīt tabulu:
+            Console.WriteLine("-------Paredzētā peļņa no dīzeļa-------");
+            Console.WriteLine("| Diena | Neste Futura 95 | Neste Futura 98 | Neste Futura D | Neste Pro Diesel |");
+            for(int i = 0 ; i < 7 ; i++)
+            {
+                Console.WriteLine($"|   {i + 1}.  | {D95.AprekinatTabulasCenas(16, maksimalaisPircejuSkKatramPumpim, aptuvenieBakasIzmeri)}| {D98.AprekinatTabulasCenas(16, maksimalaisPircejuSkKatramPumpim, aptuvenieBakasIzmeri)}| {DD.AprekinatTabulasCenas(15, maksimalaisPircejuSkKatramPumpim, aptuvenieBakasIzmeri)}| {PRO.AprekinatTabulasCenas(17, maksimalaisPircejuSkKatramPumpim, aptuvenieBakasIzmeri)}|");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Pārāk maz pircēju vai pārāk mazs bākas izmērs!");
+        }
+    }
     
     // Aprēķini mašīnai.
     /// Abstraktie override:
